fd45ec93387dd90fdc03ae547f7029f4fcec642f
Trevor Dolby <tdolby@uk.ibm.com>
2021-07-22 18:47:08 +0100

    More perf harness
    Signed-off-by: Trevor Dolby <tdolby@uk.ibm.com>

--- perf-harness/run-perf-harness.sh
+++ perf-harness/run-perf-harness.sh
@@ -18,6 +18,10 @@ echo Service returned:
 cat /tmp/curl.out
 echo
 
+echo "Running perf harrness"
+
+java -ms512M -mx512M -cp ./perfharness.jar JMSPerfHarness -tc http.HTTPRequestor -nt 10 -ss 5 -sc BasicStats -wi 10 -to 30000 -rl 30 -ws 1 -dn 1 -rb 131072 -jh localhost -jp 7800 -ot GET -ur "tea/index/0"
+
 echo "Killing server"
 ps -ef 
 ps -ef | grep IntegrationServer | cut -c8-15 
