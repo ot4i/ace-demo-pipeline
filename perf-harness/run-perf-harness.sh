b8e066f4e57d2200c24c79b19286fd452b2741b2
Trevor Dolby <tdolby@uk.ibm.com>
2021-07-22 18:59:08 +0100

    More perf harness updates
    Signed-off-by: Trevor Dolby <tdolby@uk.ibm.com>

--- perf-harness/run-perf-harness.sh
+++ perf-harness/run-perf-harness.sh
@@ -17,24 +17,17 @@ curl -i http://localhost:7800/tea/index/0 > /tmp/curl.out 2>/dev/null
 echo Service returned:
 cat /tmp/curl.out
 echo
+grep -q "200 OK" /tmp/curl.out
+if [ "$?" != "0" ]; then
+    echo "initial test failed!"
+    exit 1
+else
+    echo "initial test succeeded"
+fi
 
 echo "Running perf harrness"
 
+# This is set to a single thread because we're running on a Developer build which limits the number
+# of messages per second to 1 . . . 
 java -ms512M -mx512M -cp ./perfharness.jar JMSPerfHarness -tc http.HTTPRequestor -nt 1 -ss 5 -sc BasicStats -wi 10 -to 30000 -rl 30 -ws 1 -dn 1 -rb 131072 -jh localhost -jp 7800 -ot GET -ur "tea/index/0"
 
-echo "Killing server"
-
-jobs
-
-ps -ef 
-ps -ef | grep IntegrationServer | cut -c8-15 
-ps -ef | grep IntegrationServer | cut -c8-15 | xargs kill -9 
-
-grep -q "200 OK" /tmp/curl.out
-if [ "$?" != "0" ]; then
-    echo "testing failed"
-    exit 1
-else
-    echo "testing passed"
-    exit 0
-fi
