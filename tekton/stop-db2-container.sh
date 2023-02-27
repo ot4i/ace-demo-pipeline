9ed70c63c5271ec1d8a5decf4e8503cabeef3a37
Trevor Dolby <trevor.dolby@ibm.com>
2023-02-27 16:48:26 -0600

    Start database earlier

--- tekton/stop-db2-container.sh
+++ tekton/stop-db2-container.sh
@@ -5,7 +5,7 @@ export POD_NAME=db2-test-pod
 
 kubectl get pods -n ${NS}
 
-echo "Checking for previous component test leftovers at " `date`
+echo "Checking for component test container at " `date`
 kubectl get -n ${NS} Pod/${POD_NAME}
 if [ "$?" == "0" ]; then
     echo "Found pod; deleting it at " `date`
@@ -22,10 +22,7 @@ if [ "$?" == "0" ]; then
         fi
         sleep 5
     done
-    sleep 10
 else
     echo "No pod found at " `date`
 fi
 
-echo "Creating DB2 container for component test at " `date`
-cat /work/ace-demo-pipeline/tekton/db2-test-pod.yaml | sed 's/PASSWORD_REPLACEMENT_STRING/SecurePassw0rd/g' | kubectl apply -f -
