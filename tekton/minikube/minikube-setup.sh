b508334e67f70f808bc037bb085c0326238e7bcc
Trevor Dolby <trevor.dolby@ibm.com>
2024-03-04 20:54:43 -0600

    Minikube updates
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- tekton/minikube/minikube-setup.sh
+++ tekton/minikube/minikube-setup.sh
@@ -3,6 +3,14 @@ minikube start --insecure-registry "192.168.0.0/16"
 minikube addons enable dashboard
 minikube addons enable registry
 minikube addons enable metrics-server
+
+```
+ubuntu@minikube-20231123:~/github.com/ace-demo-pipeline$ minikube ip
+192.168.49.2
+```
+
+kubectl apply -f tekton/minikube/minikube-registry-nodeport.yaml
+
 kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
 kubectl create secret docker-registry regcred --docker-server=us.icr.io --docker-username=notused --docker-password=notused
@@ -14,7 +22,7 @@ kubectl apply -f tekton/minimal-image-build/01-ace-minimal-image-build-and-push-
 kubectl apply -f tekton/minimal-image-build/02-ace-minimal-build-image-build-and-push-task.yaml
 kubectl apply -f tekton/minimal-image-build/ace-minimal-image-pipeline.yaml
 kubectl apply -f tekton/minimal-image-build/ace-minimal-build-image-pipeline.yaml
-tkn pr delete ace-minimal-image-pipeline-run-1 -f  ; kubectl apply -f tekton/minimal-image-build/ace-minimal-image-pipeline-run-minikube.yaml
+tkn pr delete ace-minimal-image-pipeline-run-1 -f  ; kubectl apply -f tekton/minimal-image-build/ace-minimal-image-pipeline-run.yaml
 tkn pr logs ace-minimal-image-pipeline-run-1 -f
 tkn pr delete ace-minimal-build-image-pipeline-run-1 -f  ; kubectl apply -f tekton/minimal-image-build/ace-minimal-build-image-pipeline-run-minikube.yaml
 tkn pr logs ace-minimal-build-image-pipeline-run-1 -f
@@ -22,12 +30,11 @@ tkn pr logs ace-minimal-build-image-pipeline-run-1 -f
 kubectl apply -f tekton/10-maven-ace-build-task.yaml
 kubectl apply -f tekton/20-deploy-to-cluster-task.yaml
 kubectl apply -f tekton/ace-pipeline.yaml
-tkn pr delete ace-pipeline-run-1 -f  ; kubectl apply -f tekton/ace-pipeline-run-minikube.yaml
+tkn pr delete ace-pipeline-run-1 -f  ; kubectl apply -f tekton/ace-pipeline-run.yaml
 tkn pr logs ace-pipeline-run-1 -f
 
 minikube addons enable ingress
-
-
+kubectl apply -f tekton/minikube/tea-tekton-minikube-ingress.yaml
 
 
 
@@ -82,4 +89,4 @@ cat serverless/tea-tekton-knative-service.yaml | sed 's/DOCKER_REGISTRY/192.168.
 curl -LO https://github.com/knative/client/releases/download/knative-v1.11.2/kn-linux-amd64
 
 
-oc create secret docker-registry ibm-entitlement-key --docker-username=cp --docker-password=myEntitlementKey --docker-server=cp.icr.io --namespace=cp4i
+kubectl create secret docker-registry ibm-entitlement-key --docker-username=cp --docker-password=myEntitlementKey --docker-server=cp.icr.io --namespace=cp4i
