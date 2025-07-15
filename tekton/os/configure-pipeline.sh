d5e082dfd8b3a10076e93692f6f9bf1d03c0b606
Trevor Dolby <trevor.dolby@ibm.com>
2025-07-14 19:45:41 -0500

    ACE 13.0.4 updates - part 2
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- tekton/os/configure-pipeline.sh
+++ tekton/os/configure-pipeline.sh
@@ -2,9 +2,20 @@
 
 # Assumes "oc login" with a current project of "ace"
 
+# This one might fail
 kubectl delete secret regcred
+
+# Fail on error
+set -e
 kubectl create secret docker-registry regcred --docker-server=image-registry.openshift-image-registry.svc.cluster.local:5000 --docker-username=kubeadmin --docker-password=$(oc whoami -t)
+
+# Print out commands once the secret has been created
+set -x
 kubectl apply -f tekton/os/service-account.yaml
+
+# These are needed for Kaniko when running in a non-default namespace
+kubectl apply -f tekton/os/ace-scc.yaml
+oc adm policy add-scc-to-user ace-scc -z ace-tekton-service-account
 
 kubectl apply -f tekton/minimal-image-build/01-ace-minimal-image-build-and-push-task.yaml
 kubectl apply -f tekton/minimal-image-build/ace-minimal-image-pipeline.yaml
