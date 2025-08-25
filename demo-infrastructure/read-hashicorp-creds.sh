f52c96937c2fc470d572d4c08a3a1caa59bd3c40
Trevor Dolby <107267051+trevor-dolby-at-ibm-com@users.noreply.github.com>
2025-08-25 10:39:28 -0500

    Fix issue with multiple credential types

--- demo-infrastructure/read-hashicorp-creds.sh
+++ demo-infrastructure/read-hashicorp-creds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Aiming for something like
+# Aiming for YAML like
 # 
 # Credentials:
 #   jdbc:
@@ -8,7 +8,7 @@
 #       username: "USERNAME"
 #       password: "PASSWORD"
 #
-# from files that look like
+# from files with contents like
 #
 # type=jdbc
 # username=USERNAME
@@ -30,16 +30,22 @@
 echo "---"
 echo "Credentials:"
 
+# Create an associative array so we can sort by credential type.
+declare -A CRED_TYPES
 for credfile in /vault/secrets/*; do
-    export CRED_NAME=$(basename $credfile)
-    export CRED_TYPE=$(grep 'type=' $credfile | sed 's/type=//')
-    echo "  $CRED_TYPE:"
-    echo "    $CRED_NAME:"
+    CRED_TYPE=$(grep 'type=' $credfile | tr -d '\r' | sed 's/type=//')
+    CRED_TYPES[${CRED_TYPE}]="$(basename $credfile) ${CRED_TYPES[${CRED_TYPE}]}"
+done
 
-    while IFS= read -r line
-    do
-	export CRED_COMPONENT=${line%%"="*}
-	export CRED_VALUE=${line#*"="}
-        echo "      $CRED_COMPONENT: '$CRED_VALUE'"
-    done <<< $(cat $credfile | grep -v 'type=')
+# CRED_TYPES now contains the list of credentials for each type
+for credType in "${!CRED_TYPES[@]}"; do
+    # YAML spacing is important - two spaces before the type, four in front 
+    # of the name, and six for username/password/etc.
+    echo "  $credType:"
+    for credName in ${CRED_TYPES[$credType]}; do
+        echo "    ${credName}:"
+        # The original lines look like 'username=USERNAME' and we need to convert
+        # to '      username: "USERNAME"' with sed. Also need to exclude "type".
+        cat /vault/secrets/${credName} | tr -d '\r' | grep -v 'type=' | sed 's/=/: "/' | sed 's/^/      /' | sed 's/$/"/g'
+    done
 done
